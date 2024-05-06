bcb898b3194e67fb7cfe114a871621c23450c9b4
metter <markus.etter@gmail.com>
2024-05-06 18:57:57 +0200

    clicking works now

--- show_labels.py
+++ show_labels.py
@@ -3,6 +3,7 @@ from plexapi.server import PlexServer
 import os
 from dotenv import load_dotenv
 from flask_httpauth import HTTPBasicAuth
+from plexapi.exceptions import BadRequest, NotFound
 
 load_dotenv()
 
@@ -30,23 +31,27 @@ def index():
     if request.method == 'POST':
         show_id = request.form.get('show_id')
         label_action = request.form.get('label_action')
-        show = plex.fetchItem(show_id)
-        if label_action == 'add':
-            show.addLabel('KEEP')
-        elif label_action == 'remove':
-            show.removeLabel('KEEP')
+        try:
+            show = plex.fetchItem(f"/{show_id}")
+            if label_action == 'add':
+                show.addLabel('KEEP')
+            elif label_action == 'remove':
+                show.removeLabel('KEEP')
+        except (BadRequest, NotFound) as e:
+            # Handle specific exceptions, you can log the error or show a custom error message
+            # For example:
+            print(f"Error: {e}")
+            return "Error processing request", 500  # Return a 500 Internal Server Error response
         return redirect(url_for('index'))
 
     shows = plex.library.section('TV Shows').all()
     shows_info = [{
         'title': show.title,
-        'thumb': f"{PLEX_URL}{show.thumb}?X-Plex-Token={PLEX_TOKEN}",
+        'thumb': show.thumbUrl if hasattr(show, 'thumbUrl') else None,
         'ratingKey': show.ratingKey,
         'has_keep': 'KEEP' in [label.tag for label in show.labels]
     } for show in shows]
     return render_template('index.html', shows=shows_info)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
